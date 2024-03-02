66c5a00f41ec86a5d3a37c76b255192dd72da1db
Iamiakk <153729760+Iamiak@users.noreply.github.com>
2024-03-02 11:17:23 +0530

    Update main.lua

--- main.lua
+++ main.lua
@@ -22,6 +22,7 @@ local Tabs = {
   Misc = Window:AddTab({ Title = "Misc", Icon = "" }),
     Hop = Window:AddTab({ Title = "Hop", Icon = "" }),
     Setting = Window:AddTab({ Title = "Setting", Icon = "" }),
+    Visual = Window:AddTab({ Title = "Visual", Icon = "" }),
 }
 local Options = Fluent.Options
 do
@@ -6609,6 +6610,52 @@ Tabs.Misc:AddButton({
     Left.BackgroundTransparency = 1
     Left.Size = UDim2.new(.5, 0, 1, 0)
     tvk.Parent = Left
+
+Tabs.Visual:AddButton({
+        Title = "Visual levels",
+        Description = "",
+        Callback = function()
+local plr = game:GetService("Players").LocalPlayer
+local Notification = require(game:GetService("ReplicatedStorage").Notification)
+local Data = plr:WaitForChild("Data")
+local EXPFunction = require(game.ReplicatedStorage:WaitForChild("EXPFunction"))
+local LevelUp = require(game:GetService("ReplicatedStorage").Effect.Container.LevelUp)
+local Sound = require(game:GetService("ReplicatedStorage").Util.Sound)
+local LevelUpSound = game:GetService("ReplicatedStorage").Util.Sound.Storage.Other:FindFirstChild("LevelUp_Proxy") or game:GetService("ReplicatedStorage").Util.Sound.Storage.Other:FindFirstChild("LevelUp")
+function v129(p15)
+    local v130 = p15;
+    while true do
+        local v131, v132 = string.gsub(v130, "^(-?%d+)(%d%d%d)", "%1,%2");
+        v130 = v131;
+        if v132 == 0 then
+            break;
+        end;    
+    end;
+    return v130;
+end;
+
+Notification.new("<Color=Yellow>QUEST COMPLETED!<Color=/>"):Display()
+Notification.new("Earned <Color=Yellow>1,000,000,000,000 Exp.<Color=/> (+ None)"):Display()
+Notification.new("Earned <Color=Green>$25,000<Color=/>"):Display()
+plr.Data.Exp.Value = 999999999999
+plr.Data.Beli.Value = plr.Data.Beli.Value + 25000
+
+delay = 0
+count = 0
+while plr.Data.Exp.Value - EXPFunction(Data.Level.Value) > 0 do
+    plr.Data.Exp.Value = plr.Data.Exp.Value - EXPFunction(Data.Level.Value)
+    plr.Data.Level.Value = plr.Data.Level.Value + 1
+    plr.Data.Points.Value = plr.Data.Points.Value + 3
+    LevelUp({ plr })
+    Sound.Play(Sound, LevelUpSound.Value)
+    Notification.new("<Color=Green>LEVEL UP!<Color=/> (" .. plr.Data.Level.Value .. ")"):Display()
+    count = count + 1
+    if count >= 5 then
+        delay = tick()
+        count = 0
+        wait(2)
+    end
+end
 
     local Right = Instance.new("Frame", game.Players.LocalPlayer.PlayerGui.BubbleChat)
     Right.BackgroundTransparency = 1
